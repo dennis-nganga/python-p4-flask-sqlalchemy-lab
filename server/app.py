7c5008f8360c2e6104579533060be8cdceb52444
dennis-nganga <denniskimani416@gmail.com>
2023-06-22 14:16:16 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
@@ -19,16 +18,60 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if animal:
+        return f'''
+        <h1>Animal Details</h1>
+        <ul>
+            <li>Name: {animal.name}</li>
+            <li>Species: {animal.species}</li>
+            <li>Zookeeper: {animal.zookeeper.name}</li>
+            <li>Enclosure: {animal.enclosure.environment}</li>
+        </ul>
+        '''
+    return 'Animal not found.'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if zookeeper:
+        animals = zookeeper.animals
+        animal_list = '<ul>'
+        for animal in animals:
+            animal_list += f'<li>{animal.name}</li>'
+        animal_list += '</ul>'
+        return f'''
+        <h1>Zookeeper Details</h1>
+        <ul>
+            <li>Name: {zookeeper.name}</li>
+            <li>Birthday: {zookeeper.birthday}</li>
+            <li>Animals:
+                {animal_list}
+            </li>
+        </ul>
+        '''
+    return 'Zookeeper not found.'
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    if enclosure:
+        animals = enclosure.animals
+        animal_list = '<ul>'
+        for animal in animals:
+            animal_list += f'<li>{animal.name}</li>'
+        animal_list += '</ul>'
+        return f'''
+        <h1>Enclosure Details</h1>
+        <ul>
+            <li>Environment: {enclosure.environment}</li>
+            <li>Open to Visitors: {enclosure.open_to_visitors}</li>
+            <li>Animals:
+                {animal_list}
+            </li>
+        </ul>
+        '''
+    return 'Enclosure not found.'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
